cd47f2a0da6003a5586bf6eeb2eeec3aa454a3b0
Rylan-Hensley <rhensley43@ivytech.edu>
2024-06-28 16:57:32 -0400

    Add files via upload

---
+++ file.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+//reading
+fs.readFile('./docs/blog1.txt', (err, data) => {
+    if(err) {
+        console.log(err);
+    }
+    console.log(data.toString());
+});
+
+console.log('Last line');
+
+//writing
+fs.writeFile('./docs/blog2.txt', 'hello, world', () => {
+    console.log('file was written');
+});
+
+//directories
+if (!fs.existsSync('assets')){ 
+    fs.mkdir('./assets', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('folder created')
+    })
+}
+else {
+    fs.rmdir('./assets', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('folder deleted');
+    })
+}
+
+//deleting
+if (fs.existsSync('./docs/deleteme.txt')) {
+    fs.unlink('./docs/deleteme.txt', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('file deleted');
+    })
+}
